9b3ace855cb2d3fac376427b551bcd30a60bc09f
NikitaVr <nvarabei@gmail.com>
2019-11-23 23:56:37 -0500

    using neural network prediction to select a move.

--- agent.py
+++ agent.py
@@ -1,20 +1,47 @@
 import socketio
 
+import network
+
+import tensorflow as tf
+
+from tensorflow.keras.layers import Input, Concatenate, Conv2D, Flatten, Dense, concatenate
+from tensorflow.keras.models import Model
+
+import numpy as np
+
 reward = 0
+
+moves = [
+    {'type': 'move', 'direction': 'up'},
+    {'type': 'move', 'direction': 'right'},
+    {'type': 'move', 'direction': 'down'},
+    {'type': 'move', 'direction': 'left'},
+    {'type': 'use ground item'}
+]
+
+net = network.Network()
 
 # standard Python
 sio = socketio.Client(reconnection=False)
 
+
 @sio.on('get proximity')
 def on_message(data):
     global reward
+    global net
     print('I received a message!')
     reward += 1
-    if('berries' in data['dynamic'][4][4]):
-        sio.emit('action', { 'type': 'use ground item' })
-        print("USE ITEM")
-    else:
-        sio.emit('action', { 'type': 'move', 'direction': 'right' })
+    # if('berries' in data['dynamic'][4][4]):
+    #     sio.emit('action', {'type': 'use ground item'})
+    #     print("USE ITEM")
+    # else:
+    #     sio.emit('action', {'type': 'move', 'direction': 'right'})
+    sample_items = tf.cast(np.arange(81).reshape((1, 9, 9, 1)), tf.float32)
+    sample_coords = np.array([20.0, 20.0]).reshape((1, 2))
+    move = moves[net.getAction(sample_items, sample_coords)]
+    print(move)
+    sio.emit('action', move)
+
 
 @sio.on('dead of hunger')
 def on_dead():
@@ -26,5 +53,6 @@ def on_dead():
     # sio.connect('http://localhost:3000')
 
     print("RESET")
+
 
 sio.connect('http://localhost:3000')
--- network.py
+++ network.py
@@ -11,27 +11,39 @@ from tensorflow.keras.models import Model
 
 # https://github.com/CrazyBene/EvolutionaryStrategy
 
-first_input = Input(shape=(9, 9, 1))
-# first_dense = Dense(1, )(first_input)
-first_conv = Conv2D(filters=81, kernel_size=2,
-                    input_shape=(9, 9, 1))(first_input)
-first_conv = Flatten()(first_conv)
 
-second_input = Input(shape=(2, ))
-second_dense = Dense(2, )(second_input)
+class Network():
+    def __init__(self):
+        first_input = Input(shape=(9, 9, 1))
+        # first_dense = Dense(1, )(first_input)
+        first_conv = Conv2D(filters=81, kernel_size=2,
+                            input_shape=(9, 9, 1))(first_input)
+        first_conv = Flatten()(first_conv)
 
-merge_one = concatenate([first_conv, second_dense])
-# 5 possible actions currently
-merge_one = Dense(5, )(merge_one)
+        second_input = Input(shape=(2, ))
+        second_dense = Dense(2, )(second_input)
 
-model = Model(inputs=[first_input, second_input], outputs=merge_one)
+        merge_one = concatenate([first_conv, second_dense])
+        # 5 possible actions currently
+        merge_one = Dense(5, )(merge_one)
 
-model.compile(loss='mean_squared_error')
+        model = Model(inputs=[first_input, second_input], outputs=merge_one)
 
-sample_items = tf.cast(np.arange(81).reshape((1, 9, 9, 1)), tf.float32)
-sample_coords = np.array([20.0, 20.0]).reshape((1, 2))
+        model.compile(loss='mean_squared_error')
 
-print(model.predict([sample_items, sample_coords]))
+        sample_items = tf.cast(np.arange(81).reshape((1, 9, 9, 1)), tf.float32)
+        sample_coords = np.array([20.0, 20.0]).reshape((1, 2))
+
+        print(model.predict([sample_items, sample_coords]))
+
+        self.model = model
+
+    def predict(self, items, coords):
+        return self.model.predict([items, coords])
+
+    def getAction(self, items, coords):
+        return np.argmax(self.predict(items, coords))
+
 
 # combine two inputs, one is a grid of berry positions, ( later any item, just represent it as a number, 0 = nothing, 1 = berry, 2 = potion, etc.
 # Can each grid spot only contain one item ? could simplify things
